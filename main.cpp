79cf52a72a937d505ebb38284ef4484cca5f5047
Admexter <mypancakes@wp.pl>
2017-03-19 18:17:06 +0100

    Init

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+class Ship
+{
+private:
+    int ID;
+    string name;
+    int cannons;
+    int hp;
+    int masts;
+public:
+};
+
+class CrewMember
+{
+private:
+    int hp;
+    int cost;
+    int attack;
+    int defense;
+    string name;
+public:
+
+};
+
+int main()
+{
+    cout<<"------------------------------------------------------------------"
+        <<"1.Stworz statek"
+        <<"2.Zatrudnij zaloge"
+        <<"3."
+        <<"4."
+        <<"5."
+        <<"------------------------------------------------------------------";
+    return 0;
+}
